9096d367df6140a693a0bab6bffb24dbdb126aca
Gabriel Nathan <gabrielnathanmiw.info>
2022-02-09 10:32:36 -0300

    Alteracao na classe

--- ClasseA.cpp
+++ ClasseA.cpp
@@ -9,6 +9,10 @@ void MA2(){
    cout << "MA2";
 }
 
+void MA3(){
+   cout << "Alteracao a classe A partir do clone";
+}
+
 int main(){
    int A1;
    float A2;
@@ -17,6 +21,8 @@ int main(){
    MA1();
 	cout << " ";
    MA2();
-	
+	cout << endl;
+   MA3();
+   
 return 0;
 }
