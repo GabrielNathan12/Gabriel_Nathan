1331f100ead0e14a56401050aa8bf74f7f40cf59
GabrielNathan12 <76185909+GabrielNathan12@users.noreply.github.com>
2022-02-09 09:44:05 -0300

    Update ClasseB.cpp

--- ClasseB.cpp
+++ ClasseB.cpp
@@ -1,10 +1,21 @@
-include <iostream>
+#include <iostream>
 
 using namespace std;
+
+void MB1(){
+  cout << "MB1";
+}
+
+void MB2(){
+  cout << "MB2";
+}
 
 int main(){
   int B1;
   float B2;
-  
+  cin >> B1 >> B2;
+  MB1();
+  cout << " ";
+  MB2();
   return 0;
 }
