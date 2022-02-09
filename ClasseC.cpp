99c4078690cf4f4d0e7a4c6c7bf65847a97c4e9f
Gabriel Nathan <gabrielnathanmiw.info>
2022-02-09 10:36:38 -0300

    Criacao Da CLasse C

---
+++ ClasseC.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void MC1(){	
+    cout << "MC1";	
+}
+
+void MC2(){
+   cout << "MC2";
+}
+
+int main(){
+   string A1;
+   int A2;
+   
+   cin >> A1 >> A2;
+   MC1();
+	cout << " ";
+   MC2();
+	cout << endl;
+   
+return 0;
+}
