da8345d2866f6e3bdc12d0348a735b727dc4bebd
Gabriel Nathan Almeida Silva Gabriel <76185909+GabrielNathan12@users.noreply.github.com>
2022-02-09 11:45:39 -0300

    Criacao do Metodo MD4

--- ClasseD.cpp
+++ ClasseD.cpp
@@ -9,10 +9,21 @@ double MD2(double D2){
     return D2;
 }
 
+void MD3(){
+    cout << "MD3";
+}
+
+void MD4(){
+    cout << "MD4";
+}
+
 int main(){
     char D1;
     double D2;
     cin >> D1 >> D2;
 
-    cout << MD1(D1) << " " << MD2(D2);
+    cout << MD1(D1) << " " << MD2(D2) << " ";
+    MD3();
+    cout << " ";
+    MD4();
 }
