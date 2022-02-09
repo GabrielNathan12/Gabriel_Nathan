77986733166966b515376758766f81e9ec29b3ae
GabrielNathan12 <76185909+GabrielNathan12@users.noreply.github.com>
2022-02-09 09:32:01 -0300

    Update ClasseA.cpp

--- ClasseA.cpp
+++ ClasseA.cpp
@@ -1,9 +1,22 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-	int A1;
-	float A2;
+void MA1(){	
+    cout << "MA1";	
+}
 
+void MA2(){
+   cout << "MA2";
+}
+
+int main(){
+   int A1;
+   float A2;
+   
+   cin >> A1 >> A2;
+   MA1();
+	cout << " ";
+   MA2();
+	
 return 0;
 }
